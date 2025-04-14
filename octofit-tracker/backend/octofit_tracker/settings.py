e322fb6a3f97e1ee3f0728b55f2e297ec736a030
longnghoang <longnghoang@outlook.com>
2025-04-14 08:13:32 +0000

    Commit changes Step 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = 'your-secret-key'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'symmetrical-halibut-wvx47v6j5vg2w54-8000.app.github.dev']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
